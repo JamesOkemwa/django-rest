f8a988b365ddcee3b7e4ee3be4e4f9aef8bf7912
lejames <okejames55@gmail.com>
2022-09-26 12:39:04 +0300

    Echo GET data

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,5 +1,19 @@
 from django.http import JsonResponse
+import json
 
 
 def api_home(request, *args, **kwargs):
-    return JsonResponse({ "message": "Hi There. This is your django API response"})
+    body = request.body # byte string of JSON data
+    data = {}
+
+    try:
+        data = json.loads(body) # string of JSON data -> Python Dictionary
+    except:
+        pass
+
+    print(data)
+    print(request.GET) # url query params
+    data["headers"] = dict(request.headers)
+    data["content_type"] = request.content_type
+    data["params"] = dict(request.GET)
+    return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -4,7 +4,7 @@ import requests
 endpoint = 'https://httpbin.org/anything'
 endpoint = 'http://127.0.0.1:8000/api/'
 
-res = requests.get(endpoint, json={"query": "Hello World"})
+res = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World"})
 
 print(res.text)
 print(res.status_code)
