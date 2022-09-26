84ab9dacb9b06f4ed4f8b277c12080498d13a396
lejames <okejames55@gmail.com>
2022-09-26 13:21:36 +0300

    Products app. Product model -> Django model instance as API response

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,19 +1,15 @@
 from django.http import JsonResponse
 import json
 
+from products.models import Product
+
 
 def api_home(request, *args, **kwargs):
-    body = request.body # byte string of JSON data
+    model_data = Product.objects.all().order_by("?").first()
     data = {}
-
-    try:
-        data = json.loads(body) # string of JSON data -> Python Dictionary
-    except:
-        pass
-
-    print(data)
-    print(request.GET) # url query params
-    data["headers"] = dict(request.headers)
-    data["content_type"] = request.content_type
-    data["params"] = dict(request.GET)
+    if model_data:
+        data["id"] = model_data.id 
+        data["title"] = model_data.title
+        data["content"] = model_data.content 
+        data["price"] = model_data.price 
     return JsonResponse(data)
--- py_client/basic.py
+++ py_client/basic.py
@@ -6,8 +6,8 @@ endpoint = 'http://127.0.0.1:8000/api/'
 
 res = requests.get(endpoint, params={"abc": 123}, json={"query": "Hello World"})
 
-print(res.text)
-print(res.status_code)
+# print(res.text)
+# print(res.status_code)
 print(res.json())
 
 
