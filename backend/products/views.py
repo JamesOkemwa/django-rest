068814932711c46654900ed4f814470e5d99af27
lejames <okejames55@gmail.com>
2022-09-29 14:59:29 +0300

    Using function based views for create, List, Retrieve

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,4 +1,7 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from django.shortcuts import get_object_or_404
 
 from .models import Product
 from .serializers import ProductSerializer
@@ -25,6 +28,34 @@ class ProductDetailApiView(generics.RetrieveAPIView):
 
     # lookup_field = 'pk'
 
-class ProductListAPIView(generics.ListAPIView):
-    queryset = Product.objects.all()
-    serializer_class = ProductSerializer
+
+@api_view(['GET', 'POST'])
+def product_alt_view(request, pk=None, *args, **kwargs):
+    method = request.method 
+
+    if method == 'GET':
+        # detail view
+        if pk is not None:
+            # obj = Product.objects.filter(pk=pk)
+            obj = get_object_or_404(Product, pk=pk)
+            data = ProductSerializer(obj, many=False).data
+            return Response(data)
+       
+        # list view
+        queryset = Product.objects.all()
+        data = ProductSerializer(queryset, many=True).data 
+        return Response(data)
+
+    if method == 'POST':
+        # create an item
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            title = serializer.validated_data.get('title')
+            content = serializer.validated_data.get('content') or None
+
+            if content is None:
+                content = title
+            serializer.save(content=content)
+            return Response(serializer.data)
+
+
--- py_client/create.py
+++ py_client/create.py
@@ -3,8 +3,8 @@ import requests
 endpoint = 'http://127.0.0.1:8000/api/products/'
 
 data = {
-    'title': 'Chemsha',
-    'price': '32.99'
+    'title': 'Sushi',
+    'price': '49.99'
 }
 res = requests.post(endpoint, json=data)
 
