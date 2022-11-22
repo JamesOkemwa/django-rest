3ab3c544318afa30ac1b1bf6b1fa70e34af4d33c
lejames <okejames55@gmail.com>
2022-11-22 20:22:05 +0300

    Token authentication

--- py_client/create.py
+++ py_client/create.py
@@ -1,12 +1,15 @@
 import requests
 
 endpoint = 'http://127.0.0.1:8000/api/products/'
+headers = {
+    "Authorization": "Bearer 837dc5c8334697edc03b54a2a805379b48babc8b"
+}
 
 data = {
     'title': 'Pie',
     'price': '4.99'
 }
-res = requests.post(endpoint, json=data)
+res = requests.post(endpoint, json=data, headers=headers)
 
 print(res.json())
 
--- py_client/list.py
+++ py_client/list.py
@@ -1,9 +1,21 @@
 import requests
+from getpass import getpass
 
-endpoint = 'http://127.0.0.1:8000/api/products/'
+auth_endpoint = 'http://127.0.0.1:8000/api/auth/'
+username = input("What is your username? ")
+password = getpass()
 
-res = requests.get(endpoint)
+auth_response = requests.post(auth_endpoint, json={'username': username, 'password': password})
+print(auth_response.json())
 
-print(res.json())
+if auth_response.status_code == 200:
+    token = auth_response.json()['token']
+    headers = {
+        "Authorization": f"Bearer {token}"
+    }
+    endpoint = 'http://127.0.0.1:8000/api/products/'
+
+    res = requests.get(endpoint, headers=headers)
+    print(res.json())
 
 
